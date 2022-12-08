0988b7105c90a5c7a2b3167a808075d93edb4fce
Ashutoshh <asashuthegreat4@gmail.com>
2022-12-08 11:32:34 +0530

    Add files via upload

---
+++ developer/kafka-java-code/kafka-basic/src/main/java/org/example/ConsumerDemo.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+
+        //System.out.println("Hello world!");
+        // after logger use
+        log.info("hello world !");
+    }
+}
--- developer/kafka-java-code/kafka-basic/src/main/java/org/example/Mainwithcallback.java
+++ developer/kafka-java-code/kafka-basic/src/main/java/org/example/Mainwithcallback.java
@@ -1,15 +1,17 @@
 package org.example;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class Main {
-    private static  final Logger log = LoggerFactory.getLogger(Main.class.getSimpleName());
+public class Mainwithcallback {
+    private static  final Logger log = LoggerFactory.getLogger(Mainwithcallback.class.getSimpleName());
     public static void main(String[] args) {
 
         //System.out.println("Hello world!");
@@ -24,10 +26,28 @@ public class Main {
         // create producer
         KafkaProducer<String, String>  producer = new KafkaProducer<>(properties);
         // send data by creating producer record
-        ProducerRecord<String, String> producerRecord =
-                new ProducerRecord<>("ashu-logs","Hello world java code ack");
+
+
+        for (int i=0;i<10;i++){
+            ProducerRecord<String, String> producerRecord =
+            new ProducerRecord<>("ashu-logs","Hello world java code ack "+i);
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception e) {
+                    // execute everytime a message successfully sent
+                    if (e == null){
+                        log.info("received new metdata \n" +
+                                "topic : " +metadata.topic() + "\n" +
+                                "partition : "+metadata.partition() +"\n" +
+                                "Offset : " + metadata.offset());
+                    } else {
+                        log.error("Error while producing : ",e);
+                    }
+                }
+            });
+        }
         // send data synchronous way
-        producer.send(producerRecord);
+
         // above program is going to shut down to data won't be sent
         producer.flush(); // this will make code live
 
